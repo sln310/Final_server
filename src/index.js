b29a3b1d6df69d1470833733d2b3d399e5874d96
sh310310 <110559375+sh310310@users.noreply.github.com>
2023-06-13 22:30:35 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.mongdbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.log("error");
+  }
+};
+
+connectDB();
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => console.log("Server started!"));
